d75438104ed4b9a9bf063455cb133bcdacefb486
Liu-fucheng <o1574865620@gmail.com>
2025-04-11 20:42:55 +0800

    Update jsonl.sh

--- jsonl.sh
+++ jsonl.sh
@@ -11,7 +11,7 @@ CONFIG_FILE="${LOG_DIR}/config.conf"
 RULES_FILE="${LOG_DIR}/rules.txt"
 
 # GitHub相关设置
-GH_PROXY="https://ghproxy.top/"
+GH_FAST="https://ghfast.top/"
 GITHUB_REPO="Liu-fucheng/Jsonl_monitor"
 GH_DOWNLOAD_URL_BASE="https://github.com/${GITHUB_REPO}/raw/main"
 
